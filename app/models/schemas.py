fe18eacf4382e23230f5e3a7dede4b62545c5e88
QuocAnhh <anh16102004ok@gmail.com>
2025-07-29 16:32:32 +0700

    add pydantic model BookingData

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -11,3 +11,14 @@ class SmaxSendRequest(BaseModel):
     """Payload gửi ngược lại cho Smax"""
     sender_id: str
     message: str
+
+class BookingData(BaseModel):
+    id_khach: str
+    ten: str
+    sdt: str
+    dia_chi_don: str
+    dia_chi_tra: str
+    ngay_di: str
+    gio_xe_chay: str
+    so_luong_nguoi: int
+    tong_tien: int
